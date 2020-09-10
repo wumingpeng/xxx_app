ad591c19b055ac05ef307a92d85d75919dabc3ed
wumingpeng <505490956@qq.com>
2020-09-10 12:53:50 +0800

    [Fix]

---
+++ Example/Pods/Target Support Files/Pods-xxx_app_Example/Pods-xxx_app_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_xxx_app_Example : NSObject
+@end
+@implementation PodsDummy_Pods_xxx_app_Example
+@end
---
+++ Example/Pods/Target Support Files/xxx_app/xxx_app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_xxx_app : NSObject
+@end
+@implementation PodsDummy_xxx_app
+@end
